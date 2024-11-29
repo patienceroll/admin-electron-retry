e8cd31e1ea1960f5208be42357f2f446f811bb4f
zhangxianlei <1079105171@qq.com>
2024-11-29 14:01:04 +0800

    doc

--- src/client/channel/files/main.ts
+++ src/client/channel/files/main.ts
@@ -1,15 +1,26 @@
-import { app, ipcMain, shell } from "electron";
+import { app, ipcMain, Notification, shell } from "electron";
 import https from "https";
 import http from "http";
 import fs from "fs";
 import path from "path";
 
 import Framework from "src/client/main/framework";
-import { parseUrlFile } from "src/util/file/parse";
+import { formatFileSize, parseUrlFile } from "src/util/file/parse";
 
 function get(url: string) {
   return new Promise<http.IncomingMessage>((resolve, reject) => {
     https.get(url, function (response) {
+      if (response.statusCode === 200) {
+        resolve(response);
+      }
+      reject(new Error(`${response.statusCode} ${response.statusMessage}`));
+    });
+  });
+}
+
+function head(url: string) {
+  return new Promise<http.IncomingMessage>((resolve, reject) => {
+    https.get(url, { method: "HEAD" }, function (response) {
       if (response.statusCode === 200) {
         resolve(response);
       }
@@ -32,12 +43,27 @@ export default function filesMain(options: { framework: Framework }) {
     try {
       const { filename, extension } = parseUrlFile(url);
       const tempPath = path.resolve(app.getPath("temp"), `${filename}`);
-      const response = await get(url);
-      const fileStream = fs.createWriteStream(tempPath);
-      response.pipe(fileStream);
-      // 等待文件流完成
-      await promisifyStream(fileStream);
-      await shell.openPath(tempPath);
+      if (fs.existsSync(tempPath)) {
+        await shell.openPath(tempPath);
+      } else {
+        const headResponse = await head(url);
+        const contentLength = headResponse.headers["content-length"];
+        if (contentLength) {
+          if (Number(contentLength) > 2 * 1024 * 1024) {
+            const size = formatFileSize(Number(contentLength));
+            new Notification({
+              title: "正在下载预览文件",
+              body: `当前文件大小${size},预览速度可能较慢,请稍等。`,
+            }).show();
+          }
+        }
+        const response = await get(url);
+        const fileStream = fs.createWriteStream(tempPath);
+        response.pipe(fileStream);
+        // 等待文件流完成
+        await promisifyStream(fileStream);
+        await shell.openPath(tempPath);
+      }
     } catch (err: any) {
       throw new Error(err.message);
     }
--- src/util/file/parse.ts
+++ src/util/file/parse.ts
@@ -22,3 +22,12 @@ export function parseUrlFile(url: string) {
     extension: extension,
   };
 }
+
+/** 解析文件大小为  Bytes", "KB", "MB", "GB", "TB" */
+export function formatFileSize(bytes: number) {
+  if (bytes === 0) return "0 Bytes";
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(2))} ${sizes[i]}`;
+}
