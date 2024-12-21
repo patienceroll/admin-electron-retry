4d7e9584393302ba68fe940c523c8ec9527b534e
刘白涛01 <10728871+liu-baitao-01@user.noreply.gitee.com>
2024-12-21 16:03:01 +0800

    销售合同列表

--- src/apps/admin/pages/sales/sales-contract/index.tsx
+++ src/apps/admin/pages/sales/sales-contract/index.tsx
@@ -14,13 +14,14 @@ import Search from "src/framework/component/search";
 import PageWrapper from "src/framework/component/page-wrapper";
 import SearchAction from "src/framework/component/search/search-action";
 import AddressFormSearch from "src/framework/component/adress-form-search";
+
+import useSearchTable from "src/hooks/use-search-table";
+import useColumnState from "src/hooks/use-column-state";
+
 import openWindow from "src/util/open-window";
 import { watherMap } from "src/apps/admin/api/general";
 import styled, { useTheme } from "styled-components";
 
-//hooks
-import useSearchTable from "src/hooks/use-search-table";
-import useColumnState from "src/hooks/use-column-state";
 //主体接口
 import { getSalesContractList } from "src/apps/admin/api/sales-contract";
 //关联接口
