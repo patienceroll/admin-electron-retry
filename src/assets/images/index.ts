feed26f6fceb97de73745db97371db50de3210c5
刘白涛01 <10728871+liu-baitao-01@user.noreply.gitee.com>
2024-12-23 16:04:48 +0800

    销售合同业务图标

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,19 @@
 import login from "./login.jpg";
 import bg from "./bg.png";
 import approval from "./approval.png";
+import red_star from "./red_star.png";
+import replenish from "./replenish.png";
+import urgent from "./urgent.png";
+import overdue from "./overdue.png";
+import dispatch from "./dispatch.png";
 
 export default {
   login,
   bg,
   approval,
+  red_star,
+  replenish,
+  urgent,
+  overdue,
+  dispatch,
 };
