ea868f0e75c9e9e3fd6b2198a4af227bc9569ba8
zhangxianlei <1079105171@qq.com>
2025-01-06 17:01:21 +0800

    编辑销售合同

---
+++ src/apps/admin/pages/sales/sales-contract/components/edit-sale-contract-detail/index.tsx
@@ -0,0 +1,154 @@
+import React, { useEffect } from "react";
+import ProTable from "@ant-design/pro-table";
+import { useTheme } from "styled-components";
+
+import { getSalesContractDetailList } from "src/apps/admin/api/sales-contract-detail";
+import useSearchTable from "src/hooks/use-search-table";
+
+export default function (props: Pick<SalesContract, "id">) {
+  const { id } = props;
+  const theme = useTheme();
+
+  const table = useSearchTable(getSalesContractDetailList);
+
+  const column = table.column([
+    {
+      title: "产品",
+      key: "material",
+      fixed:'left',
+      renderText: (_, row) => row.material?.name,
+    },
+    {
+      title: "外径",
+      key: "o_diameter",
+      renderText: (_, row) => row.material_sku?.o_diameter,
+    },
+    {
+      title: "壁厚",
+      key: "wall_thickness",
+      renderText: (_, row) => row.material_sku?.wall_thickness,
+    },
+    {
+      title: "内涂层厚度",
+      key: "i_coat_thickness",
+      renderText: (_, row) => row.material_sku?.i_coat_thickness,
+    },
+    {
+      title: "外涂层厚度",
+      key: "o_coat_thickness",
+      renderText: (_, row) => row.material_sku?.o_coat_thickness,
+    },
+    {
+      title: "长度",
+      key: "length",
+      renderText: (_, row) => row.material_sku?.length,
+    },
+    {
+      title: "连接方式",
+      key: "connection_type",
+      renderText: (_, row) => row.material_sku?.connection_type,
+    },
+    {
+      title: "钢卷类型",
+      key: "steel_type",
+      renderText: (_, row) => row.material_sku?.steel_type,
+    },
+    {
+      title: "材质",
+      key: "texture",
+      renderText: (_, row) => row.material_sku?.texture,
+    },
+    {
+      title: "内涂层颜色",
+      key: "i_coat_color",
+      renderText: (_, row) => row.material_sku?.i_coat_color,
+    },
+    {
+      title: "外涂层颜色",
+      key: "o_coat_color",
+      renderText: (_, row) => row.material_sku?.o_coat_color,
+    },
+    {
+      title: "拓展属性",
+      dataIndex: "standard_attributes",
+      // render(_, row) {
+      //   return (
+      //     <AttrSnapshoot attr_snapshoot={row.material_sku?.attr_snapshoot} />
+      //   );
+      // },
+    },
+    // {
+    //   title: "执行属性",
+    //   key: "execution_attributes",
+    //   dataIndex: "execution_attributes",
+    //   width: 340,
+    //   render(_, row) {
+    //     return (
+    //       <AttrSnapshoot
+    //         attr_snapshoot={row.attr_snapshoot}
+    //       />
+    //     );
+    //   },
+    // },
+    {
+      title: "执行标准",
+      dataIndex: "standard",
+    },
+    // {
+    //   title: "单价信息",
+    //   width: 235,
+    //   render: (_, row) => {
+    //     return row.line_unit.map((i) => (
+    //       <div
+    //         style={{
+    //           display: "flex",
+    //           textAlign: "left",
+    //           marginLeft: 20,
+    //         }}
+    //         key={i.id}
+    //       >
+    //         <Badge
+    //           status={i.is_execute ? "success" : "default"}
+    //           style={{ flexShrink: 0 }}
+    //         />
+    //         &nbsp;
+    //         <span style={{ flex: 1 }}>
+    //           {i.num}&nbsp;&nbsp;(&nbsp;{i.unit}&nbsp;)
+    //         </span>
+    //         <span style={{ flex: 1 }}>{new Money(i.price).toCNY()}</span>
+    //       </div>
+    //     ));
+    //   },
+    // },
+    {
+      title: "金额",
+      dataIndex: "amount",
+      valueType: "money",
+    },
+    {
+      title: "备注",
+      dataIndex: "remark",
+      renderText: (_, row) => row.remark,
+    },
+  ]);
+
+  useEffect(() => {
+    table.extraParams.current.sales_contract_id = id;
+    table.reload();
+  }, []);
+
+  return (
+    <ProTable
+      style={{ marginTop: theme.margin }}
+      rowKey="id"
+      search={false}
+      loading={table.loading}
+      options={table.options}
+      dataSource={table.dataSource}
+      pagination={table.pagination}
+      onChange={table.onChange}
+      columns={column}
+      scroll={{ x: table.measureColumnWidth(column), y: 500 }}
+    />
+  );
+}
--- src/apps/admin/pages/sales/sales-contract/edit.tsx
+++ src/apps/admin/pages/sales/sales-contract/edit.tsx
@@ -1,25 +1,33 @@
 import styled, { useTheme } from "styled-components";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
-import { Card, Col, Form, Row } from "antd";
+import { Card, Col, FloatButton, Form, Row } from "antd";
 import {
   ProFormDatePicker,
   ProFormDigit,
   ProFormRadio,
   ProFormSelect,
   ProFormText,
+  ProFormTextArea,
   ProFormTreeSelect,
 } from "@ant-design/pro-form";
 import dayjs from "dayjs";
 
 import PageWrapper from "src/framework/component/page-wrapper";
-import { getSalesContract } from "src/apps/admin/api/sales-contract";
+import {
+  editSalesContract,
+  getSalesContract,
+} from "src/apps/admin/api/sales-contract";
 import Title from "src/framework/component/title";
 import useOption from "src/hooks/use-option";
 import { getProjectOption } from "src/apps/admin/api/project";
 import { getClientOption } from "src/apps/admin/api/client";
 import { getClientContactOptions } from "src/apps/admin/api/client-concat";
 import useStaffTree from "src/b-hooks/use-staff-tree";
+import BusinessFile from "src/b-components/business-file";
+import EditSaleContractDetail from "./components/edit-sale-contract-detail";
+import Icon from "src/framework/component/icon";
+import SaveSvg from "src/assets/svg/保存.svg";
 
 function Edit(props: StyledWrapComponents) {
   const { className } = props;
@@ -35,6 +43,20 @@ function Edit(props: StyledWrapComponents) {
 
   const theme = useTheme();
   const [form] = Form.useForm();
+
+  function submit() {
+    return form.validateFields().then((store) => {
+      editSalesContract({
+        id,
+        ...store,
+        sign_date: store.sign_date.format("YYYY-MM-DD"),
+        advance_amount: store.advance_amount || 0,
+      }).then(() => {
+        window.parent.postMessage("success");
+        window.close();
+      });
+    });
+  }
 
   function getDetail() {
     getSalesContract({ id }).then((res) => {
@@ -89,12 +111,17 @@ function Edit(props: StyledWrapComponents) {
               <ProFormText
                 label="合同"
                 name="name"
-                required
+                rules={[{ required: true }]}
                 placeholder="请输入合同名称"
               />
             </Col>
             <Col flex="350px">
-              <ProFormText label="编号" name="code" required readonly />
+              <ProFormText
+                label="编号"
+                name="code"
+                rules={[{ required: true }]}
+                readonly
+              />
             </Col>
             <Col flex="350px">
               <ProFormRadio.Group
@@ -110,7 +137,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormSelect<Project>
                 label="项目"
                 name="project_id"
-                required
+                rules={[{ required: true }]}
                 placeholder="请选择项目"
                 options={project.list}
                 fieldProps={{
@@ -127,7 +154,7 @@ function Edit(props: StyledWrapComponents) {
               >
                 label="客户"
                 name="client_id"
-                required
+                rules={[{ required: true }]}
                 placeholder="请选择客户"
                 options={client.list}
                 fieldProps={{
@@ -163,7 +190,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormText
                 label="签约地点"
                 name="sign_address"
-                required
+                rules={[{ required: true }]}
                 placeholder="请输入签约地点"
               />
             </Col>
@@ -171,7 +198,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormDatePicker
                 label="签约日期"
                 name="sign_date"
-                required
+                rules={[{ required: true }]}
                 placeholder="请选择签约日期"
                 fieldProps={{ format: "YYYY-MM-DD", style: { width: "100%" } }}
               />
@@ -180,7 +207,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormTreeSelect
                 label="签约人"
                 name="sign_staff_id"
-                required
+                rules={[{ required: true }]}
                 placeholder="请选择签约人"
                 fieldProps={{
                   treeData: treeOptions,
@@ -192,7 +219,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormTreeSelect
                 label="负责人"
                 name="staff_id"
-                required
+                rules={[{ required: true }]}
                 placeholder="请选择负责人"
                 fieldProps={{
                   treeData: treeOptions,
@@ -204,7 +231,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormDigit
                 label="税率"
                 name="tax_rate"
-                required
+                rules={[{ required: true }]}
                 fieldProps={{
                   min: 0,
                   max: 100,
@@ -219,7 +246,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormDigit
                 label="预付款比例"
                 name="advance_ratio"
-                required
+                rules={[{ required: true }]}
                 fieldProps={{
                   min: 0,
                   max: 100,
@@ -234,7 +261,7 @@ function Edit(props: StyledWrapComponents) {
               <ProFormDigit
                 label="质保金比例"
                 name="quality_ratio"
-                required
+                rules={[{ required: true }]}
                 fieldProps={{
                   min: 0,
                   max: 100,
@@ -249,16 +276,92 @@ function Edit(props: StyledWrapComponents) {
               <ProFormRadio.Group
                 label="厂家配送"
                 name="is_factory_dispatch"
-                required
+                rules={[{ required: true }]}
                 options={[
                   { label: "否", value: 0 },
                   { label: "是", value: 1 },
                 ]}
               />
             </Col>
+            <Col flex="700px">
+              <ProFormTextArea
+                name="remark"
+                label="备注"
+                placeholder="请输入备注"
+              />
+            </Col>
           </Row>
         </Card>
+
+        <Title style={{ marginTop: theme.margin }}>结算、付款及开票</Title>
+
+        <Card style={{ marginTop: theme.margin }}>
+          <Row gutter={[theme.padding, theme.padding]}>
+            <Col flex="350px">
+              <ProFormSelect<any>
+                label="甲方结算人员"
+                name="settle_client_contact_id"
+                placeholder="请选择甲方结算人员"
+                options={clientConcat.list}
+                fieldProps={{
+                  fieldNames: { label: "name", value: "id" },
+                  filterOption: true,
+                  showSearch: true,
+                  optionFilterProp: "name",
+                }}
+              />
+            </Col>
+            <Col flex="350px">
+              <ProFormTreeSelect
+                label="乙方结算人员"
+                name="settle_staff_id"
+                rules={[{ required: true }]}
+                placeholder="请选择乙方结算人员"
+                fieldProps={{
+                  treeData: treeOptions,
+                  treeNodeFilterProp: "title",
+                }}
+              />
+            </Col>
+
+            <Col flex="700px">
+              <ProFormTextArea
+                name="settle_type"
+                label="结算方式"
+                placeholder="请输入结算方式"
+                rules={[{ required: true }]}
+              />
+            </Col>
+          </Row>
+        </Card>
+
+        <Title style={{ marginTop: theme.margin }}>附件信息</Title>
+
+        {detail && (
+          <Card style={{ marginTop: theme.margin }}>
+            <BusinessFile
+              id={id}
+              service="sales-contract"
+              identify="合同附件"
+              isCover={0}
+              files={detail.file["合同附件"]}
+            />
+          </Card>
+        )}
+
+        <Title style={{ marginTop: theme.margin }}>产品明细</Title>
+        <EditSaleContractDetail id={id} />
       </Form>
+
+      {detail && (
+        <FloatButton
+          icon={<Icon icon={SaveSvg} />}
+          tooltip="保存"
+          shape="square"
+          style={{ insetInlineEnd: 24 }}
+          onClick={submit}
+        />
+      )}
     </PageWrapper>
   );
 }
